3bf7de0843dfbb82cb36f84fdb68617f5aedd19f
Yashwanth Pindi <pindiyashwanth@gmail.com>
2024-12-26 14:23:23 +0100

    First Commit

---
+++ 1_HelloWorld.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World" << "\n";
+    string name, str;
+    int age;
+    /*
+    cout << "Enter your name:";
+    cin >> str;
+    cout << "Hello " << str << "!";
+    return 0;
+    cout << "Enter your name and age:";
+    cin >> name >> age;
+    cout << "Name : " << name << endl;
+    cout << "Age : " << age << endl;
+    cout << "----------------" << endl;
+    */
+    cout << "Type for name is: " << sizeof(name);
+    cout << "Type for name is: " << sizeof(age);
+}
